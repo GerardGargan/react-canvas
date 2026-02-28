e9e28c1ecea568af13dcebbdc4e4276fb372f0d8
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2026-02-28 17:15:24 +0000

    Remove old code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Canvas from "./pages/canvas";
 export function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Fishbone />} /> */}
       <Route path="/" element={<Canvas />} />
       <Route path="*" element={<>Not found</>} />
     </Routes>
